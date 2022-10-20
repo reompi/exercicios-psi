98b8d10f3fd3276d5eecfb1f40d2ec33a7dc6edd
reompi <114647549+reompi@users.noreply.github.com>
2022-10-20 08:23:33 +0100

    Add files via upload

---
+++ exericicio 2/exericicio 2/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Console
+Module Module1
+    Dim num1, num2, num3 As Decimal
+    Sub Main()
+        WriteLine("insira um numero")
+        num1 = ReadLine()
+        WriteLine("insira um numero")
+        num2 = ReadLine()
+        WriteLine("insira um numero")
+        num3 = ReadLine()
+
+        WriteLine("a media dos seus numeros é " & (num1 + num2 + num3) / 3)
+        ReadLine()
+    End Sub
+
+End Module
