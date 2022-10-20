eb9b774001effac9b80a69624787f46aacd85395
reompi <114647549+reompi@users.noreply.github.com>
2022-10-20 08:24:40 +0100

    Add files via upload

---
+++ exercicio 3/exercicio 3/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Console
+Module Module1
+    Dim base1, base2 As Decimal
+    Sub Main()
+        WriteLine("insira a base")
+        base1 = ReadLine()
+        WriteLine("insira o expoente")
+        base2 = ReadLine()
+
+
+        WriteLine("a potencia dos seus numeros é " & base1 ^ base2)
+        ReadLine()
+    End Sub
+
+End Module
---
+++ exercicio 4/exercicio 4/Module1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Console
+Module Module1
+    Dim cateto1, cateto2 As Decimal
+
+    Sub Main()
+        WriteLine("insira o valor do cateto")
+        cateto1 = ReadLine()
+        WriteLine("insira o valor do outro cateto")
+        cateto2 = ReadLine()
+        WriteLine("o valor da hipotenusa é " & Math.Sqrt(cateto1 ^ 2 + cateto2 ^ 2))
+        ReadLine()
+
+
+
+
+
+    End Sub
+
+End Module
