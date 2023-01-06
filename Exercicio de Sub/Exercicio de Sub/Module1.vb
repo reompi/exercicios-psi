96102bce49614134159a85b1e307ba1b76e8ad69
reompi <114647549+reompi@users.noreply.github.com>
2023-01-06 08:24:33 +0000

    Add files via upload

---
+++ Exercicio de Sub/Exercicio de Sub/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Console
+Module Module1
+    Dim valor1, valor2 As Decimal
+    Sub Main()
+
+        WriteLine("Insire 2 valores")
+        valor1 = ReadLine()
+        valor2 = ReadLine()
+        Soma(valor1, valor2, 0)
+        ReadLine()
+    End Sub
+    Sub Soma(n1 As Decimal, n2 As Decimal, n As Decimal)
+        n = n1 + n2
+        WriteLine(n)
+
+    End Sub
+
+End Module
---
+++ função raiz quadrada/função raiz quadrada/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Console
+Module Module1
+    Dim numero As Integer
+    Sub Main()
+        WriteLine("Introduza um número")
+        numero = ReadLine()
+        operação(numero)
+        ReadLine()
+    End Sub
+    Sub operação(n As Integer)
+        n = Math.Sqrt(n)
+        WriteLine(n)
+
+    End Sub
+End Module
