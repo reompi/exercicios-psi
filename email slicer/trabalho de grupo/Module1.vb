ca6d27c5118962e6ccad4243b42be850717e2c56
reompi <114647549+reompi@users.noreply.github.com>
2023-01-06 08:22:04 +0000

    Add files via upload

---
+++ email slicer/trabalho de grupo/Module1.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Console
+Module Module1
+
+    'Email slicer
+
+    Dim email, emailslicer, endereço, username As String
+    Dim arroba, final, inicio As Integer
+    Dim contador As Integer = 0
+
+    Sub Main()
+        WriteLine("##### EMAIL SLICER #####
+Introduza o email para receber o resultado sem o enderesso")
+        email = ReadLine()
+        arroba = InStr(email, "@")
+        contador = 0
+
+        Validor()
+
+        email = email.Replace(" ", "")
+
+
+        inicio = 1
+
+
+
+
+        emailslicer = Mid(email, inicio, arroba)
+        username = emailslicer.Replace("@", "")
+        endereço = Mid(email, arroba, Len(email))
+
+        Clear()
+        WriteLine("USERNAME = " & username & "          ENDEREÇO = " & endereço & "   presse ENTER para recomeçar o programa")
+
+        ReadLine()
+        Clear()
+        Main()
+    End Sub
+
+
+
+
+    Sub Validor()
+
+        For i = 0 To email.Length - 1
+            If email(i) = "@" Then
+                contador = contador + 1
+            End If
+
+        Next
+
+
+        If contador > 1 Then
+            Do
+                contador = 0
+                Clear()
+                WriteLine("Email inválido, Insira um email válido com 1 arroba")
+                email = ReadLine()
+
+                For i = 0 To email.Length - 1
+                    If email(i) = "@" Then
+                        contador = contador + 1
+                    End If
+
+                Next
+
+
+            Loop Until contador = 1
+
+
+        End If
+    End Sub
+End Module
---
+++ exercicio 32/exercicio 32/Module1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Console
+Module Module1
+    Dim numerodehoras As Decimal
+    Dim valor, valor2, valortotal, troco, trocorestante As Decimal
+    Sub Main()
+        WriteLine("Insira o tempo que esteve no estacionamento em horas")
+        numerodehoras = ReadLine()
+        If numerodehoras <= 0 Then
+            Do
+                WriteLine("Número inválido, introduza outro valor")
+                numerodehoras = ReadLine()
+            Loop While numerodehoras <= 0
+        End If
+        valorparque()
+        WriteLine("O Montate a pagar é " & valor & " euro introduza o pagamento")
+        valor2 = ReadLine()
+        valortotal = valortotal + valor2
+        troco = valortotal - valor
+        trocorestante = troco * -1
+
+        If valor2 < valor And valor2 > 0 Then
+            Do
+                If valor2 < 0 Then
+                    WriteLine("Valor inválido introduza outro valor")
+                    valor2 = ReadLine()
+                Else
+
+
+                    troco = valortotal - valor
+                    trocorestante = troco * -1
+                    WriteLine("inire o restante ...  Restante a pagar = " & trocorestante)
+                    valor2 = ReadLine()
+                    valortotal = valortotal + valor2
+                End If
+
+            Loop While valortotal < valor
+
+
+
+        End If
+        WriteLine("Valor pago com sucesso    TROCO = " & troco)
+        ReadLine()
+
+    End Sub
+    Sub valorparque()
+
+        Select Case numerodehoras
+            Case 1
+                valor = "0.8"
+            Case 2 To 3
+                valor = 0.6 * numerodehoras
+            Case 4 To 5
+                valor = 0.5 * numerodehoras
+            Case 6 To 7
+                valor = 0.4 * numerodehoras
+            Case Else
+                valor = 0.3 * numerodehoras
+        End Select
+
+    End Sub
+End Module
