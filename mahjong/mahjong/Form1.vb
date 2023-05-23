0bb2cfd99e1b228f18abed583b6c4e89dc28eec0
reompi <114647549+reompi@users.noreply.github.com>
2023-05-23 12:37:19 +0100

    Add files via upload

---
+++ mahjong/mahjong/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Dim adicionartiles As New adicionartiles
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        adicionartiles.criarpicturebox()
+    End Sub
+End Class
---
+++ mahjong/mahjong/adicionartiles.vb
@@ -0,0 +1,27 @@
+﻿Public Class adicionartiles
+
+    Dim tiles(9, 7) As PictureBox
+    Sub criarpicturebox()
+        ReDim tiles(9, 7)
+        Dim ticker As Integer = 0
+
+        For y As Integer = 0 To 7
+            For x As Integer = 0 To 9
+                Me.tiles(x, y) = New PictureBox()
+                With Me.tiles(x, y)
+                    .Top = (135 + y * 50)
+                    .Left = (60 + x * 100)
+                    .Name = "PictureBox" & ticker
+                    .Size = New System.Drawing.Size(100, 50)
+                    .TabIndex = 0
+                    .TabStop = False
+                End With
+
+                Form1.Controls.Add(tiles(x, y))
+                ticker += 1
+            Next
+        Next
+
+    End Sub
+
+End Class
