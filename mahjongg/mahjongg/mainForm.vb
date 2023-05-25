2ee4f62b25521ff4cb79d46844394559ea731533
reompi <114647549+reompi@users.noreply.github.com>
2023-05-25 23:11:43 +0100

    Add files via upload

---
+++ mahjongg/mahjongg/mainForm.vb
@@ -0,0 +1,12 @@
+﻿Public Class mainForm
+    Dim pecas As New Template
+
+
+
+    Private Sub mainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        pecas.criarPictureBoxes()
+
+
+
+    End Sub
+End Class
---
+++ mahjongg/mahjongg/variaveisGlobais.vb
@@ -0,0 +1,8 @@
+﻿Module variaveisGlobais
+    Public tilesLayer(14, 7) As PictureBox
+    Public tilesLayer1(14, 7) As PictureBox
+    Public tilesLayer2(14, 7) As PictureBox
+    Public tilesLayer3(14, 7) As PictureBox
+    Public tilesLayer4(14, 7) As PictureBox
+    Public tilesList As New ArrayList
+End Module
